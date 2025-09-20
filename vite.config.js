88c7aa38edc410aeea9322ee8eb5501011050e9e
adarsh0504 <39239062+adarsh0504@users.noreply.github.com>
2025-09-20 19:59:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-export default defineConfig({ base: '/srf-website/', plugins: [react()] })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: '/srf-website/',
+  plugins: [react()],
+});
